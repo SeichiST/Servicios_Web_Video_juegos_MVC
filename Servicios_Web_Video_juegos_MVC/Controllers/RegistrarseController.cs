de9f9c421ba5cd7dbbd8ab53520c181810aa65d9
Rodrigo <dc.4231253@gmail.com>
2026-08-18 19:06:55 -0500

    Registrar usuario

---
+++ Servicios_Web_Video_juegos_MVC/Controllers/RegistrarseController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Servicios_Web_Video_juegos_MVC.Dto;
+using System.Text;
+
+namespace Servicios_Web_Video_juegos_MVC.Controllers
+{
+    public class RegistrarseController : Controller
+    {
+        private readonly string _apiregistrarse =
+            "https://localhost:7017/api/RegistrarseAPI";
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registrarse (RegistrarseDto dto)
+        {
+           using (HttpClient cliente = new HttpClient())
+            {
+                string json = JsonConvert.SerializeObject(dto);
+                var contenido = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var respuesta = await cliente.PostAsync(_apiregistrarse, contenido);
+
+                if (!respuesta.IsSuccessStatusCode)
+                {
+                    string respuestaJson = await respuesta.Content.ReadAsStringAsync();
+
+                    try
+                    {
+                        var resultado = JsonConvert.DeserializeObject<dynamic>(respuestaJson);
+                        ViewBag.Mensaje = resultado?.mensaje ?? "No se pudo realizar el registro";
+                    }
+                    catch
+                    {
+                        ViewBag.Mensaje = "No se pudo realizar el registro";
+                    }
+                    return View("Index", dto);
+                }
+
+                ViewBag.Mensaje = "Registro realizado correctamente";
+                return RedirectToAction("Index", "Login");
+            }
+
+        }
+    }
+}
---
+++ Servicios_Web_Video_juegos_MVC/Dto/RegistrarseDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Servicios_Web_Video_juegos_MVC.Dto
+{
+    public class RegistrarseDto
+    {
+        public string Apellidos { get; set; } = null!;
+        public string Nombres { get; set; } = null!;
+        public string Dni { get; set; } = null!;
+        public string Direccion { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+        public DateOnly FechaNacimiento { get; set; }
+        public string Sexo { get; set; } = null!;
+        public string Correo { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
